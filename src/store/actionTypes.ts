4abeb6481565c269cd6e9bc80401259b0031d3a0
Luis Castillo <ldcastillocisnero@gmail.com>
2020-08-29 18:35:37 -0400

    Store added

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,11 @@
+export const START_SEARCH_MOVIE: string = "START_SEARCH_MOVIE";
+export const SUCCESS_SEARCH_MOVIE: string = "SUCCESS_SEARCH_MOVIE";
+export const FAIL_SEARCH_MOVIE: string = "FAIL_SEARCH_MOVIE";
+
+export const START_GET_MOVIES: string = "START_GET_MOVIES";
+export const SUCCESS_GET_MOVIES: string = "SUCCESS_GET_MOVIES";
+export const FAIL_GET_MOVIES: string = "FAIL_GET_MOVIES";
+
+export const START_GET_MOVIE_BY_ID: string = "START_GET_MOVIE_BY_ID";
+export const SUCCESS_GET_MOVIE_BY_ID: string = "SUCCESS_GET_MOVIE_BY_ID";
+export const FAIL_GET_MOVIE_BY_ID: string = "FAIL_GET_MOVIE_BY_ID";
---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./rootReducer";
+import sagas from "./sagas";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore(initialState = {}) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancer(applyMiddleware(sagaMiddleware))
+  );
+
+  sagaMiddleware.run(sagas);
+
+  return store;
+}
